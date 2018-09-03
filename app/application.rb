43a150ad3cb082f1c61cdfcdba0b352413fc3f0d
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-03 23:52:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,15 +11,17 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      item = @@items.find{|i| i.name == item_name}
+      if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
 
-    elsif !req.path.match(/items/)
+    else
         resp.write "Route not found"
         resp.status = 404
-    else
-      resp.write "Item not found"
-      resp.status = 400
+    
     end
 
 
