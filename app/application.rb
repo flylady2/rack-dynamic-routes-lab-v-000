5824262ebf3414db73a6da676760a672b5338792
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-03 23:50:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
 require 'pry'
 class Application
 
-  @@items = []
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     #binding.pry
 
-    if req.path.match(/items/) && @@items != []
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
       item = @@items.find{|i| i.name == item_name}
