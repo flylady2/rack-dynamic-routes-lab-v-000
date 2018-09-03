8de215bd45be0ab68cd35b532f9a66bada79a591
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-03 23:33:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry
+    #binding.pry
 
     if req.path.match(/testing/) 
       resp.write "Route not found"
